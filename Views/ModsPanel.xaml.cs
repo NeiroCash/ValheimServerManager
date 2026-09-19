23dbb1975706770c1c17a8e690c49f5d8d9257c5
NeiroCash <neirocash@gmail.com>
2026-09-19 10:44:39 +0200

    Resolve WPF UserControl ambiguity in mods panel

--- Views/ModsPanel.xaml.cs
+++ Views/ModsPanel.xaml.cs
@@ -2,7 +2,7 @@ using System.Windows.Controls;
 
 namespace ValheimServerManager.Views;
 
-public partial class ModsPanel : UserControl
+public partial class ModsPanel : System.Windows.Controls.UserControl
 {
     public ModsPanel()
     {
