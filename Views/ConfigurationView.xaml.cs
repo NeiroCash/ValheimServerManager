278c417839121d007a9f808a02efe25395ddfca7
NeiroCash <neirocash@gmail.com>
2026-09-19 10:41:20 +0200

    Resolve WPF UserControl ambiguity in configuration view

--- Views/ConfigurationView.xaml.cs
+++ Views/ConfigurationView.xaml.cs
@@ -2,7 +2,7 @@ using System.Windows.Controls;
 
 namespace ValheimServerManager.Views;
 
-public partial class ConfigurationView : UserControl
+public partial class ConfigurationView : System.Windows.Controls.UserControl
 {
     public ConfigurationView() => InitializeComponent();
 }
