82a946ef50c98a6b495d684fb1f4d2a65d5d531f
NeiroCash <neirocash@gmail.com>
2026-09-19 10:31:00 +0200

    Track selected sidebar page for navigation highlighting

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel;
 using ValheimServerManager.Views;
 
 namespace ValheimServerManager.ViewModels;
@@ -6,12 +5,15 @@ namespace ValheimServerManager.ViewModels;
 public sealed class MainWindowViewModel : ViewModelBase
 {
     private object _currentPage;
+    private bool _isDashboardSelected;
+    private bool _isConfigurationSelected;
 
     public MainWindowViewModel()
     {
         Dashboard = new DashboardViewModel();
         Configuration = new ConfigurationViewModel();
         _currentPage = Dashboard;
+        _isDashboardSelected = true;
 
         ShowDashboardCommand = new RelayCommand(() => CurrentPage = Dashboard);
         ShowConfigurationCommand = new RelayCommand(() => CurrentPage = Configuration);
@@ -26,11 +28,33 @@ public sealed class MainWindowViewModel : ViewModelBase
         private set
         {
             if (ReferenceEquals(_currentPage, value)) return;
+
             _currentPage = value;
+            IsDashboardSelected = ReferenceEquals(value, Dashboard);
+            IsConfigurationSelected = ReferenceEquals(value, Configuration);
             OnPropertyChanged();
         }
     }
 
+    public bool IsDashboardSelected
+    {
+        get => _isDashboardSelected;
+        private set => Set(ref _isDashboardSelected, value);
+    }
+
+    public bool IsConfigurationSelected
+    {
+        get => _isConfigurationSelected;
+        private set => Set(ref _isConfigurationSelected, value);
+    }
+
     public RelayCommand ShowDashboardCommand { get; }
     public RelayCommand ShowConfigurationCommand { get; }
+
+    private void Set<T>(ref T field, T value, [System.Runtime.CompilerServices.CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return;
+        field = value;
+        OnPropertyChanged(propertyName);
+    }
 }
